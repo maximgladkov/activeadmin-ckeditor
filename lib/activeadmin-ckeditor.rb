dae2773f3891cde028921973f1e02a3e6dbfe9f5
Maxim Gladkov <maksim.gladkov@gmail.com>
2013-10-01 15:50:32 +0400

    misc

--- lib/activeadmin-ckeditor.rb
+++ lib/activeadmin-ckeditor.rb
@@ -1,3 +1,3 @@
-# require "activeadmin-ckeditor/version"
-# require "activeadmin-ckeditor/engine"
-# require "ckeditor"
+require "activeadmin-ckeditor/version"
+require "activeadmin-ckeditor/engine"
+require "ckeditor"
