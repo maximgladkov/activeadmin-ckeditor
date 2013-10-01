751ebf1b873cb846e7271995a46b124f659d7292
Maxim Gladkov <maksim.gladkov@gmail.com>
2013-10-01 16:04:58 +0400

    misc

--- lib/activeadmin-ckeditor/engine.rb
+++ lib/activeadmin-ckeditor/engine.rb
@@ -2,6 +2,7 @@ module Activeadmin
   module Ckeditor
     class Engine < ::Rails::Engine
       initializer 'activeadmin-ckeditor.load_static_assets' do |app|
+        p "***** #{ root } *****"
         middleware.insert_before ::ActionDispatch::Static, ::ActionDispatch::Static, "#{ root }/app"
       end
     end
--- lib/activeadmin-ckeditor/version.rb
+++ lib/activeadmin-ckeditor/version.rb
@@ -1,5 +1,5 @@
 module Activeadmin
   module Ckeditor
-    VERSION = "0.0.13"
+    VERSION = "0.0.14"
   end
 end
